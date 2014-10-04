85ebdc8bf0cdb53f5213479a01aed5059c308979
raido-ddr <raido.ddr@gmail.com>
2014-10-04 10:02:43 +0300

    Add dropped file type validation

--- OSISP_1/DragDrop.cpp
+++ OSISP_1/DragDrop.cpp
@@ -10,11 +10,27 @@ void ProcessDragRequest(HWND hWnd, HDROP hDrop) {
 		DragQueryFile(hDrop, 0, fileName, MAX_PATH);
 		if (MessageBox(hWnd, L"Close current file?", L"Drag and Drop", MB_YESNO) == IDYES)
 		{
-			LoadBitmapFromFile(hWnd, fileName);
+			SHFILEINFO shfi;
+			SHGetFileInfo(
+				fileName,
+				0,
+				&shfi,
+				sizeof(SHFILEINFO),
+				SHGFI_TYPENAME
+				);
+			if (_tccmp(shfi.szTypeName, L"Точечный рисунок") == 0) 
+			{
+				LoadBitmapFromFile(hWnd, fileName);
+			}
+			else
+			{
+				MessageBox(hWnd, L"Inapproptiate file format.", L"Drag and Drop", MB_OK);
+			}	
 		}
 	}
 	else if (fileCount > 1) {
 		MessageBox(hWnd, L"Please, drop only one file at a time.", L"Drag and Drop", MB_OK);
 	}
 	DragFinish(hDrop);
+	
 }
